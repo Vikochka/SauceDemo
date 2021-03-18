361c9f7e38b6220406947a704521ec1d9be790a4
Viktoria <62216160+Vikochka@users.noreply.github.com>
2021-03-18 20:46:04 +0300

    fixed linux chrome

--- src/test/java/utils/CapabilitiesGenerator.java
+++ src/test/java/utils/CapabilitiesGenerator.java
@@ -10,9 +10,9 @@ public class CapabilitiesGenerator {
         String os = System.getProperty("os.name").toLowerCase();
         System.out.println("Operational system: " + os + "; Driver path: " + driverPath);   //УЗНАЕМ КАКАЯ ОП.СИСТ
         if (os.contains("win")) {
-            System.setProperty("webdriver.chrome.driver", driverPath + "/chromedriver.exe");
+            System.setProperty("webdriver.chrome.driver", driverPath + "chromedriver.exe");
         } else if (os.contains("mac")) {
-            System.setProperty("webdriver.chrome.driver", driverPath + "/chromedriver");
+            System.setProperty("webdriver.chrome.driver", driverPath + "chromedriver");
         } else {
             System.setProperty("webdriver.chrome.driver", driverPath + "linux/chromedriver");
         }
